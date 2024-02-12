f653c5b98cfb886ee72e9cf8ef7ca388a044e1c7
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2024-02-12 21:40:47 +0200

    Chnaged footer size

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -7,25 +7,25 @@ import { AiFillInstagram } from "react-icons/ai"
 
 const Footer = () => {
   return (
-    <div className="w-[100%] h-64 bg-gray mt-16 py-10 mx-auto">
+    <div className="w-[100%] h-64 bg-gray mt-16 py-10">
 
       <h2 className="font-bold text-4xl m-auto text-center mt-8">Socials</h2>
 
       <div className="flex flex-row space-between my-auto mx-auto w-fit">
 
-        <div className="w-20 m-4">
+        <div className="w-16 my-4">
           <a href="https://github.com/NiphoMathibela"><AiFillGithub className='text-4xl' /></a>
         </div>
 
-        <div className="w-20 m-4">
+        <div className="w-16 my-4">
           <a href="https://www.linkedin.com/in/nipho/"><AiFillLinkedin className='text-4xl' /></a>
         </div>
 
-        <div className="w-20 m-4">
+        <div className="w-16 my-4">
           <a href="https://twitter.com/Niph0Mathibela"><AiFillTwitterCircle className='text-4xl' /></a>
         </div>
 
-        <div className="w-18 m-4">
+        <div className="w-16 my-4">
           <a href= "https://www.instagram.com/nipho.mathibela/"><AiFillInstagram className='text-4xl' /></a>
         </div>
       </div>
