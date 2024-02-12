ae581fded42adedaf431031059b9a5008cdc2e92
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2024-02-12 20:56:08 +0200

    Changed project card sizes

--- portfolio/src/components/ProjectCard.jsx
+++ portfolio/src/components/ProjectCard.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const ProjectCard = (props) => {
   let url = props.url
   return (
-    <div className="w-full sm:w-[42%] h-96 rounded-xl m-4 drop-shadow-4xl bg-black pointer-events-auto overflow-scroll" onClick= {() => window.open(url, "_blank")}>
+    <div className="w-full sm:w-[42%] h-[405px] rounded-xl m-4 drop-shadow-4xl bg-black pointer-events-auto py-2 " onClick= {() => window.open(url, "_blank")}>
       <h3 className="font-bold text-lg text-center mb-6 text-green">{props.title}</h3>
-      <img src= {props.img} alt= "Preview image of my Portfolio" className= "w-11/12 block mx-auto object-fit rounded-md"/>
+      <img src= {props.img} alt= "Preview image of my Portfolio" className= "w-11/12 block mx-auto object-fit rounded-md max-h-[140px]"/>
       <div className="m-1 text-white text-sm mx-auto p-5">
-        <p>{props.description}</p>
-        <p className=" font-semibold text-base my-3 text-green">Technologies Used</p>
+        <p className=" h-20 my-2 overflow-y-auto ">{props.description}</p>
+        <p className=" font-semibold text-base my-2 text-green">Technologies Used</p>
         <p>{props.tech}</p>
       </div>
     </div>
