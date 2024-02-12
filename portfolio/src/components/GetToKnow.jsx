097ebe31ca5d0c7455c345672fc6bd9e0b7556fd
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2024-02-12 19:33:08 +0200

    Changed get to know me text

--- portfolio/src/components/GetToKnow.jsx
+++ portfolio/src/components/GetToKnow.jsx
@@ -5,9 +5,11 @@ const GetToKnow = () => {
     <div className="m-auto w-8/12">
       <h2 className="font-bold text-3xl text-center mb-6">Get To Know Me</h2>
       <p>
-        I am a third year university student studying IT, I have already gained a strong foundation in software development
-        and a strong passion for creating innovative solutions.
-        I am constantly learning and staying up to date with the latest technologies in the industry. Not only am I a hard worker and dedicated to delivering quality work, but I also have a laid back and humorous personality that makes me a great team player.
+        I am a recent graduate of the University of Johannesburg, where I earned a Diploma in Business
+        Information Technology. My studies equipped me with a comprehensive understanding of both
+        IT infrastructure and business operations, preparing me to bridge the gap between these two
+        critical domains. I am highly motivated and eager to apply my theoretical knowledge and
+        practical skills to contribute to a dynamic organization.
       </p>
     </div>
   );
