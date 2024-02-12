f2189a066eff9a3f3c75858aa91b44e91bb1cb02
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2024-02-12 20:40:34 +0200

    Changed project card

--- portfolio/src/components/ProjectCard.jsx
+++ portfolio/src/components/ProjectCard.jsx
@@ -3,9 +3,14 @@ import React from "react";
 const ProjectCard = (props) => {
   let url = props.url
   return (
-    <div className="w-44 h-44 rounded-xl m-4 drop-shadow-4xl bg-black pointer-events-auto" onClick= {() => window.open(url, "_blank")}>
-      <h3 className="font-bold text-xl text-center mb-6 text-white">{props.title}</h3>
-      <img src= {props.img} alt= "Preview image of my Portfolio" className= "w-11/12 block mx-auto object-fit"/>
+    <div className="w-full sm:w-[42%] h-96 rounded-xl m-4 drop-shadow-4xl bg-black pointer-events-auto overflow-scroll" onClick= {() => window.open(url, "_blank")}>
+      <h3 className="font-bold text-lg text-center mb-6 text-green">{props.title}</h3>
+      <img src= {props.img} alt= "Preview image of my Portfolio" className= "w-11/12 block mx-auto object-fit rounded-md"/>
+      <div className="m-1 text-white text-sm mx-auto p-5">
+        <p>{props.description}</p>
+        <p className=" font-semibold text-base my-3 text-green">Technologies Used</p>
+        <p>{props.tech}</p>
+      </div>
     </div>
   );
 };
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -5,24 +5,26 @@ const Projects = () => {
 
   const projectsArray = [
     {
-        title: "My Portfolio",
-        description: "",
-        url: "https://nipho-mathibela.onrender.com/",
-        img: "https://github.com/NiphoMathibela/my-portfolio/blob/master/portfolio/src/assets/Portfolio.png?raw=true"
+      title: "My Portfolio",
+      description: "A personal website that showcases my skills and experience as a full-stack developer. The website should serve as an online portfolio, attracting potential clients and employers while demonstrating my proficiency in front-end and back-end development.",
+      tech: "React, TailwindCSS, NodeJS",
+      url: "https://nipho-mathibela.onrender.com/",
+      img: "https://github.com/NiphoMathibela/my-portfolio/blob/master/portfolio/src/assets/Portfolio.png?raw=true"
     },
     {
       title: "Lomaway Website",
-        description: "Comapny website for a constructtio comapany",
-        url: "https://lomaway.onrender.com/",
-        img: "https://github.com/NiphoMathibela/my-portfolio/blob/master/portfolio/src/assets/Lomaway.png?raw=true"
+      description: " This project aims to create a user-friendly and informative website for a construction company. The website should effectively attract potential clients, showcase the company's capabilities, and establish trust in its expertise.",
+      tech: "HTML, CSS, JavaScript",
+      url: "https://lomaway.onrender.com/",
+      img: "https://github.com/NiphoMathibela/my-portfolio/blob/master/portfolio/src/assets/Lomaway.png?raw=true"
     }
-]
+  ]
 
   return (
     <>
       <h2 className="font-bold text-3xl text-center mt-16">My Recent Work</h2>
-      <div className="m-auto w-8/12 mt-6 mb-6 flex flex-wrap justify-center">
-        {projectsArray.map(project => <ProjectCard title= {project.title} img= {project.img} url= {project.url}/>)}
+      <div className="m-auto w-[80%] sm:w-9/12 mt-6 mb-6 flex flex-wrap justify-center">
+        {projectsArray.map(project => <ProjectCard title={project.title} img={project.img} url={project.url} description={project.description} tech={project.tech} />)}
       </div>
     </>
   );
