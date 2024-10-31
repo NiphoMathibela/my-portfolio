ffe9cfbc3a7d1e4426769eef3a89ce4ca788f1f0
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2024-10-31 11:28:59 +0000

    Updated footer

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,32 +4,38 @@ import { AiFillGithub } from "react-icons/ai"
 import { AiFillLinkedin } from "react-icons/ai"
 import { AiFillTwitterCircle } from "react-icons/ai"
 import { AiFillInstagram } from "react-icons/ai"
+import Tooltip from './Tooltip'
 
 const Footer = () => {
   return (
-    <div className="w-[100%] h-64 bg-gray mt-16 py-10">
-
-      <h2 className="font-bold text-4xl m-auto text-center mt-8">Socials</h2>
-
-      <div className="flex flex-row space-between my-auto mx-auto w-fit">
-
-        <div className="w-16 my-4">
-          <a href="https://github.com/NiphoMathibela"><AiFillGithub className='text-4xl' /></a>
-        </div>
-
-        <div className="w-16 my-4">
-          <a href="https://www.linkedin.com/in/nipho/"><AiFillLinkedin className='text-4xl' /></a>
-        </div>
-
-        <div className="w-16 my-4">
-          <a href="https://twitter.com/Niph0Mathibela"><AiFillTwitterCircle className='text-4xl' /></a>
-        </div>
-
-        <div className="w-16 my-4">
-          <a href= "https://www.instagram.com/nipho.mathibela/"><AiFillInstagram className='text-4xl' /></a>
-        </div>
+    <>
+      <div className='h-24'>
+      <Tooltip />
       </div>
-    </div>
+    </>
+    // <div className="w-[100%] h-64 bg-gray mt-16 py-10">
+
+    //   <h2 className="font-bold text-4xl m-auto text-center mt-8">Socials</h2>
+
+    //   <div className="flex flex-row space-between my-auto mx-auto w-fit">
+
+    //     <div className="w-16 my-4">
+    //       <a href="https://github.com/NiphoMathibela"><AiFillGithub className='text-4xl' /></a>
+    //     </div>
+
+    //     <div className="w-16 my-4">
+    //       <a href="https://www.linkedin.com/in/nipho/"><AiFillLinkedin className='text-4xl' /></a>
+    //     </div>
+
+    //     <div className="w-16 my-4">
+    //       <a href="https://twitter.com/Niph0Mathibela"><AiFillTwitterCircle className='text-4xl' /></a>
+    //     </div>
+
+    //     <div className="w-16 my-4">
+    //       <a href= "https://www.instagram.com/nipho.mathibela/"><AiFillInstagram className='text-4xl' /></a>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
